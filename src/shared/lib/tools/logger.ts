59f5250c74c34365f46388e9bb9244e61ec46ea8
Jayden <2077780946@qq.com>
2026-02-05 18:22:35 +0800

    refactor: replace winston with consola for logging functionality to improve logging capabilities and simplify logger configuration

--- src/shared/lib/tools/logger.ts
+++ src/shared/lib/tools/logger.ts
@@ -1,7 +1,3 @@
-import winston from "winston"
+import { consola } from "consola"
 
-export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-  transports: [new winston.transports.Console()],
-})
+export const logger = consola.withTag("app")
